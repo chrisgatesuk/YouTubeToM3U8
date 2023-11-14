503249a48caf7682d450224903bdf42983590eb8
chrisgatesuk <32849942+chrisgatesuk@users.noreply.github.com>
2023-11-14 10:48:44 +0000

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -88,7 +88,7 @@ Grabs the live-streaming M3U8 file
 
 
     if '.m3u8' not in response or stream_info.status_code != 200:
-        print(url)
+        print(url: str)
         return
     end = response.find('.m3u8') + 5
     tuner = 100
